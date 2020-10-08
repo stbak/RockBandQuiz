3b5197f06ae652505334373df8dbb318f3eacb46
HM\STBAK <stefan.barkelius@hm.com>
2020-10-08 10:48:06 +0200

    Added background music and also started with showing faulty guesses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,28 @@
-import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class Main {
+public class Main extends Intro {
+
+    public Main() throws IOException {
+    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
         Terminal terminal = terminalFactory.createTerminal();
+
         terminal.setCursorVisible(true);
 
         int terminalWidth = terminal.getTerminalSize().getColumns();
         int terminalHeight = terminal.getTerminalSize().getRows();
 
         //some fancy presentation stuff
-        String gamename = "Guess the rockband";
+        String gamename = "\\m/ Guess the rockband \\m/";
         String gameinfo = "Press < to end game";
         Character[] c = new Character[gamename.length()];
         int gameNameStartingPoint = (terminalWidth/2) - (gamename.length()/2);
@@ -29,22 +33,26 @@ public class Main {
 
         for (int i = 0; i < c.length; i++) {
             terminal.setCursorPosition(gameNameStartingPoint, 1); //gameName on first row
+            //terminal.setForegroundColor(TextColor.ANSI.RED);
             terminal.putCharacter(gamename.charAt(i));
             gameNameStartingPoint++;
 
         }
         terminal.flush();
         for (int i = 0; i < gameinfo.length(); i++) {
-            terminal.setCursorPosition(infoStartingPoint, 24);
+            terminal.setCursorPosition(infoStartingPoint, 24); //end game info
             terminal.putCharacter(gameinfo.charAt(i));
             infoStartingPoint++;
 
         }
-
         terminal.flush();
+
+        // Example of playing background music in new thread, just use Music class and these 2 lines:
+        Thread thread = new Thread(new Music());
+        thread.start();
+
+
         //get the band and write out as many _ as char's in bandname
-       // GetTheBand band = new GetTheBand();
-       // String currentBand = band.guessTheBand().trim();
         GetTheBand band = new GetTheBand();
         String currentBand = band.guessTheBand();
         int xStart = 2;
@@ -87,6 +95,7 @@ public class Main {
             Character c2 = keyStroke.getCharacter();
             System.out.println("keyStroke.getKeyType(): " + type
                     + " keyStroke.getCharacter(): " + c2);
+
             if (c2 == Character.valueOf('<')) {
                 continueReadingInput = false;
                 terminal.close();
@@ -96,8 +105,28 @@ public class Main {
             Guess guess = new Guess(c2, currentBand);
             guess.theGuess();
 
-            hitsInBandNameString = guess.getHits().size();
+            hitsInBandNameString = guess.getHits().size(); //same char can be on several places in bandname
             System.out.println("hits size: " + hitsInBandNameString);
+
+            int noHitXPos = 2; //this is where we start putting incorrect char's on X-axis
+            final int noHitYPos = yStart + 4; //this is the row we always use for incorrect guesses
+
+            //char[] to keep all faulty guesses, must be as big as allowed number of guesses
+            ArrayList<Character> faultyGuesses = new ArrayList<>();
+
+            //PRINT OUT GUESSES
+            if (hitsInBandNameString == 0) {
+                faultyGuesses.add(c2);
+                for (Character faulty: faultyGuesses
+                     ) {
+                    terminal.setCursorPosition(noHitXPos, noHitYPos);
+                    terminal.putCharacter(faulty);
+                    noHitXPos++;
+                }
+                terminal.setCursorPosition(2, yStart+2); //cursor for user input
+                terminal.flush();
+
+            }
 
             for (int i = 0; i < hitsInBandNameString; i++) { //place guess on correct location(s)
                 terminal.setCursorPosition(orgXStart + (guess.getHits().get(i) * 2),yStart);
